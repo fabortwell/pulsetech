4b9e09224ca1ca0a7dd27054a96c2f84f7332fd8
Brian Sitati <briansit93@gmail.com>
2024-12-17 22:49:43 +0300

    added postgres for production database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_17_111823) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
